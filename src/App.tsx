cbe6d75784d89196a48dd4fa773b998359a53d19
Alex <alexis.mar.schmidt@gmail.com>
2023-08-01 10:15:55 +0200

    do structure, set types

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+type FormElement = React.FormEvent<HTMLFormElement>;
+
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState<string>("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleSubmit = (e: FormElement) => {
+    e.preventDefault();
+  };
+
+  const addTask = (name: string) => {
+    const newTasks = [...tasks, { name, done: false }];
+    setTasks(newTasks);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setNewTask(e.target.value)} />
+        <button>Save</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
