d1c36cc87e139fdbad21a7d6fe40dc116ee8dd86
Alex <alexis.mar.schmidt@gmail.com>
2023-08-02 19:54:09 +0200

    add tasks to array

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
 
 type FormElement = React.FormEvent<HTMLFormElement>;
+interface ITask {
+  name: string;
+  done: boolean;
+}
 
 function App(): JSX.Element {
   const [newTask, setNewTask] = useState<string>("");
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState<ITask[]>([]);
 
   const handleSubmit = (e: FormElement) => {
     e.preventDefault();
+    addTask(newTask);
+    console.log(tasks);
   };
 
   const addTask = (name: string) => {
-    const newTasks = [...tasks, { name, done: false }];
+    const newTasks: ITask = [...tasks, { name, done: false }];
     setTasks(newTasks);
   };
 
